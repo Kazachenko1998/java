856bf55360f54c2703366029a4f0f6aa6bfa7413
Kazachenko1998 <kazachenko.1998k@gmail.com>
2017-03-01 21:09:02 +0300

    Graph вариант№8 еще тест

--- test/Tests.java
+++ test/Tests.java
@@ -254,6 +254,75 @@ public class Tests {
     }
 
     @Test
+    @Tag("renameTrack")
+    void deleteTrack() {
+        ArrayList<ArrayList<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(2);
+        matr1.add(-1);
+        matr1.add(null);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(8);
+        matr2.add(0);
+        matr2.add(-1);
+        matr2.add(2);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1111);
+        matr3.add(-10);
+        matr3.add(0);
+        matr3.add(999);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(0);
+        matr4.add(4);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("Kost");
+        nnnn.add("ghbd");
+        nnnn.add("100");
+        nnnn.add("rrr");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        ArrayList<ArrayList<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0, 0);
+        matr12.add(1, 2);
+        matr12.add(2, -1);
+        matr12.add(3, -1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(0, 8);
+        matr22.add(1, 0);
+        matr22.add(2, -1);
+        matr22.add(3, 2);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(0, 1111);
+        matr32.add(1, -10);
+        matr32.add(2, 0);
+        matr32.add(3, 999);
+        matr0.add(matr32);
+        ArrayList<Integer> matr42 = new ArrayList<>();
+        matr42.add(-1);
+        matr42.add(0);
+        matr42.add(4);
+        matr42.add(0);
+        matr0.add(matr42);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add(0, "Kost");
+        nnnn2.add(1, "ghbd");
+        nnnn2.add(2, "100");
+        nnnn2.add(3, "rrr");
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graphTest, graph.deleteTrack("rrr", "Kost"));
+    }
+
+    @Test
     @Tag("input")
     void input() {
         ArrayList<ArrayList<Integer>> matr = new ArrayList<>();
